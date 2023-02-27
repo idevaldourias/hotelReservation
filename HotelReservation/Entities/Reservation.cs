6e929c0e862c4c7c3167356e84062e53fc05cf3f
Idevaldo Urias <idevaldorodrigues@gmail.com>
2023-02-27 12:17:23 -0300

    Adicionar arquivos de projeto.

---
+++ HotelReservation/Entities/Reservation.cs
@@ -0,0 +1,58 @@
+﻿using HotelReservation.Entities.Exceptions;
+
+namespace HotelReservation.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+
+        public DateTime Checkin { get; set; }
+
+        public DateTime Checkout { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            if (checkin <= DateTime.Now)
+            {
+                throw new DomainException("The check-in date cannot be earlier than the current date");
+            } 
+            else if (checkout <= checkin)
+            {
+                throw new DomainException("The check-out date must be after the check-in date");
+            }
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = Checkout.Subtract(Checkin);
+            return duration.Days;
+        }
+
+        public void UpdateDates(DateTime chekin, DateTime checkout)
+        {
+            if (chekin <= Checkin || checkout <= Checkout)
+            {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            if (checkout <= chekin)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+            Checkin = chekin;
+            Checkout = checkout;
+        }
+
+        public override string ToString()
+        {
+            return "Room " + RoomNumber 
+                + "; Check-In: " 
+                + Checkin.ToString("dd/MM/yyyy") + "; Check-Out: "
+                + Checkout.ToString("dd/MM/yyyy") + "; " + Duration() + " nights.";
+        }
+    }
+}
---
+++ HotelReservation/Program.cs
@@ -0,0 +1,43 @@
+﻿using HotelReservation.Entities;
+using HotelReservation.Entities.Exceptions;
+
+namespace HotelReservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(roomNumber, checkin, checkout);
+
+                Console.Write("Reservation: " + reservation);
+                Console.WriteLine();
+
+                Console.WriteLine("Enter data to update the reservation:");
+
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkin, checkout);
+
+                Console.Write("Reservation: " + reservation);
+            } catch (DomainException ex)
+            {
+                Console.WriteLine("Error in reservation: " + ex.Message);
+            }
+        }
+    }
+}
